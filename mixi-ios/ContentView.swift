13f6a954d98f5b9660bda152470008211652aa96
Kou Yamamoto <yama.121020@gmail.com>
2021-05-24 21:16:08 +0900

    Xcodeプロジェクトの追加

---
+++ mixi-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mixi-ios
+//
+//  Created by kou yamamoto on 2021/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mixi-ios/mixi_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixi_iosApp.swift
+//  mixi-ios
+//
+//  Created by kou yamamoto on 2021/05/24.
+//
+
+import SwiftUI
+
+@main
+struct mixi_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
